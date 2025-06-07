369077764ebce6c5770ec40f6ae11375a353c6a6
a.chiodo <alexbypa@gmail.com>
2025-06-07 15:13:31 +0200

    Envelop in progress

--- LoggerHelperDemo/LoggerHelperDemo/Endpoints/UserSyncEndpoint.cs
+++ LoggerHelperDemo/LoggerHelperDemo/Endpoints/UserSyncEndpoint.cs
@@ -1,4 +1,6 @@
-﻿using LoggerHelperDemo.Entities;
+﻿using CSharpEssentials.LoggerHelper;
+using LoggerHelperDemo.Entities;
+using LoggerHelperDemo.Models;
 using LoggerHelperDemo.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +14,11 @@ public class UserSyncEndpoint : IEndpointDefinition {
     }
     // Single Responsibility: qui solo il mapping e il result
     private async Task<IResult> SyncUsers([FromQuery] int page, IUserService service) {
+        loggerExtension<IRequest>.TraceAsync(new LoggerRequest(), Serilog.Events.LogEventLevel.Information, null, "Loaded LoggerHelper");
+        if (!string.IsNullOrEmpty(loggerExtension<IRequest>.CurrentError))
+            return Results.BadRequest(loggerExtension<IRequest>.CurrentError);
+        if (loggerExtension<IRequest>._errors != null)
+            return Results.BadRequest(string.Join(",", loggerExtension<IRequest>._errors.ToList().Select(item => $"{item.SinkName}: {item.ErrorMessage}")));
         var users = await service.SyncUsersAsync(page);
         return Results.Ok(users);
     }
