df209dbc679fa8bbcde028cefcd0068d29f34a2e
a.chiodo <alexbypa@gmail.com>
2025-06-13 21:19:03 +0200

    Work In progress !!! ( Finish Chsarp:HttpHelper )

--- CSharpEssentials.HttpHelper/HttpClientHandlerLogging.cs
+++ CSharpEssentials.HttpHelper/HttpClientHandlerLogging.cs
@@ -30,8 +30,9 @@ public class HttpClientHandlerLogging : DelegatingHandler {
 
         int totRetry = request.Headers.Contains("X-Retry-Attempt") ? int.Parse(request.Headers.GetValues("X-Retry-Attempt").FirstOrDefault()) : 0;
         TimeSpan RateLimitTimeSpanElapsed = request.Headers.Contains("X-RateLimit-TimeSpanElapsed") ? TimeSpan.Parse(request.Headers.GetValues("X-RateLimit-TimeSpanElapsed").FirstOrDefault()) : TimeSpan.Zero;
+        
         await _events.InvokeAll(request, response, totRetry, RateLimitTimeSpanElapsed);
-
+        _events.ClearAll();
         return response;
     }
     public static string GetPageName(HttpRequestMessage request) {
@@ -45,14 +46,19 @@ public class HttpClientHandlerLogging : DelegatingHandler {
 public interface IHttpRequestEvents {
     void Add(Func<HttpRequestMessage, HttpResponseMessage, int, TimeSpan, Task> callback);
     Task InvokeAll(HttpRequestMessage req, HttpResponseMessage res, int totRetry, TimeSpan RateLimitTimeSpanElapsed);
+    void ClearAll();
 }
 
 public class HttpRequestEvents : IHttpRequestEvents {
     private readonly List<Func<HttpRequestMessage, HttpResponseMessage, int, TimeSpan, Task>> _callbacks = new();
     public void Add(Func<HttpRequestMessage, HttpResponseMessage, int, TimeSpan, Task> c) => _callbacks.Add(c);
+    public void ClearAll() {
+        _callbacks.Clear();
+    }
     public Task InvokeAll(HttpRequestMessage request, HttpResponseMessage response, int retryCount, TimeSpan rateLimitTimeSpanElapsed) {
         var tasks = _callbacks
             .Select(cb => cb(request, response, retryCount, rateLimitTimeSpanElapsed));
+        
         return Task.WhenAll(tasks);
     }
 }
