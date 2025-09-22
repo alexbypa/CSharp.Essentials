75b8dd51a69c58ee588f46d787531e31ef206b20
a.chiodo <alexbypa@gmail.com>
2025-09-22 16:48:15 +0200

    AI Dashboard in progress

---
+++ CSharpEssentials.LoggerHelper.AI/Infrastructure/AiServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CSharpEssentials.LoggerHelper.AI.Infrastructure {
+    public static class AiServiceExtensions {
+        public static IServiceCollection AddAiServices(this IServiceCollection services) {
+
+            services.AddControllers();
+            return services;
+        }
+    }
+}
